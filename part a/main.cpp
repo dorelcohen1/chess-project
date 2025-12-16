5d18ddb849b02e48880ad90ca4cd5ce5fcbafd8b
Dorel Cohen <yourEmail@example.com>
2025-12-16 22:15:11 +0200

    first runing code -dorel

--- part a/main.cpp
+++ part a/main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     std::cout << "Chess pieces module loaded successfully." << std::endl;
+    // here will be the tests
+    std::cin.get(); // this keeps the console window open until a key is pressed
     return 0;
 }
 
